9170ad133a01ea8023d11d0de7c75da2b68e18ca
esernaalonso <edusernalonso@gmail.com>
2016-03-21 21:05:07 -0400

    DEV-11 Adding miniconda as a dist source in the master

---
+++ esa/common/python/dist/miniconda2/Scripts/conda-env-script.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    import sys
+    from conda_env.cli.main import main
+
+    sys.exit(main())
---
+++ esa/common/python/dist/miniconda2/Scripts/conda-script.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    import sys
+    from conda.cli.main import main
+
+    sys.exit(main())
